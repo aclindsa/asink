bebf4efe6ab759f5b6e75594284dc29e9599feb4
Aaron Lindsay <aaron@aclindsay.com>
2013-10-10 21:44:19 -0400

    Multi-thread event processing in normal context

--- asink/context_normal.go
+++ asink/context_normal.go
@@ -8,11 +8,16 @@ import (
 	"asink"
 )
 
+const NUM_NORMAL_WORKERS = 50
+
 type NormalContext struct {
 	globals           *AsinkGlobals
 	localUpdatesChan  chan *asink.Event
 	remoteUpdatesChan chan *asink.Event
 	exitChan          chan int
+	workerError       chan error
+	workerExit        chan int
+	workerExited      chan int
 }
 
 func NewNormalContext(globals *AsinkGlobals, localChan chan *asink.Event, remoteChan chan *asink.Event, exitChan chan int) *NormalContext {
@@ -24,7 +29,8 @@ func NewNormalContext(globals *AsinkGlobals, localChan chan *asink.Event, remote
 	return nc
 }
 
-func (nc *NormalContext) Run() error {
+func (nc *NormalContext) runWorker() {
+	defer func() { nc.workerExited <- 0 }()
 	for {
 		select {
 		case event := <-nc.localUpdatesChan:
@@ -32,13 +38,15 @@ func (nc *NormalContext) Run() error {
 			err := ProcessLocalEvent(nc.globals, event)
 			if err != nil {
 				if e, ok := err.(ProcessingError); !ok || e.ErrorType != TEMPORARY {
-					return err
+					nc.workerError <- err
+					continue
 				} else {
 					//if error was temporary, retry once
 					event.LocalStatus = 0
 					err := ProcessLocalEvent(nc.globals, event)
 					if err != nil {
-						return err
+						nc.workerError <- err
+						continue
 					}
 				}
 			}
@@ -46,18 +54,50 @@ func (nc *NormalContext) Run() error {
 			err := ProcessRemoteEvent(nc.globals, event)
 			if err != nil {
 				if e, ok := err.(ProcessingError); !ok || e.ErrorType != TEMPORARY {
-					return err
+					nc.workerError <- err
+					continue
 				} else {
 					//if error was temporary, retry once
 					event.LocalStatus = 0
 					err := ProcessRemoteEvent(nc.globals, event)
 					if err != nil {
-						return err
+						nc.workerError <- err
+						continue
 					}
 				}
 			}
-		case <-nc.exitChan:
-			return ProcessingError{EXITED, nil}
+		case <-nc.workerExit:
+			return
 		}
 	}
+}
+func (nc *NormalContext) Run() error {
+	nc.workerExit = make(chan int)
+	nc.workerExited = make(chan int)
+	nc.workerError = make(chan error)
+
+	//start all the goroutines
+	for i := 0; i < NUM_NORMAL_WORKERS; i++ {
+		go nc.runWorker()
+	}
+
+	var err error
+	//wait until an error or we exit
+	select {
+	case <-nc.exitChan:
+		err = ProcessingError{EXITED, nil}
+	case err = <-nc.workerError:
+	}
+
+	//notify all the goroutines we're exiting
+	for i := 0; i < NUM_NORMAL_WORKERS; i++ {
+		nc.workerExit <- 0
+	}
+
+	//wait until they're finished
+	for i := 0; i < NUM_NORMAL_WORKERS; i++ {
+		<-nc.workerExited
+	}
+
+	return err
 }
