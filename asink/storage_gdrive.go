907709f2ce5035efc3cf0c261e350705927a4ae4
Aaron Lindsay <aaron@aclindsay.com>
2014-03-30 14:36:06 -0400

    Work on getting google drive working

--- asink/storage_gdrive.go
+++ asink/storage_gdrive.go
@@ -8,22 +8,32 @@ import (
 	"code.google.com/p/goauth2/oauth"
 	"code.google.com/p/goconf/conf"
 	"code.google.com/p/google-api-go-client/drive/v2"
+	"code.google.com/p/google-api-go-client/googleapi"
+	"code.google.com/p/mxk/go1/flowcontrol"
 	"errors"
 	"fmt"
 	"io"
+	"math/rand"
+	"net"
 	"net/http"
+	"strings"
+	"time"
+	"reflect"
 )
 
 const GDRIVE_CLIENT_ID = "1006560298028.apps.googleusercontent.com"
 const GDRIVE_CLIENT_SECRET = "2iTpEeN76RQK5KKF6ut1TCpV"
+const GDRIVE_MAX_CONNECTIONS = 1 //should this be configurable?
 
 type GDriveStorage struct {
+	connectionsChan chan int
 	cachefile string
 	directory string
 	auth_code string
 	folderid  string
 	transport *oauth.Transport
 	service   *drive.Service
+	max_kbps int64
 }
 
 func NewGDriveStorage(config *conf.ConfigFile) (*GDriveStorage, error) {
@@ -79,6 +89,9 @@ func NewGDriveStorage(config *conf.ConfigFile) (*GDriveStorage, error) {
 	}
 
 	folderlist, err := s.Files.List().Q("mimeType = 'application/vnd.google-apps.folder' and title = '" + directory + "'").Do()
+	if err != nil {
+		return nil, err
+	}
 
 	if len(folderlist.Items) < 1 {
 		//try to create a folder named 'directory'
@@ -104,7 +117,52 @@ func NewGDriveStorage(config *conf.ConfigFile) (*GDriveStorage, error) {
 	gs.service = s
 	gs.transport = transport
 	gs.folderid = folderid
+	gs.connectionsChan = make(chan int, GDRIVE_MAX_CONNECTIONS)
+	gs.max_kbps = 512 //TODO make configurable
 	return gs, nil
+}
+
+func (gs *GDriveStorage) fileExists(hash string) (bool, error) {
+	folderlist, err := gs.service.Files.List().Q("mimeType = 'application/pgp-encrypted' and title = '" + hash + "' and '" + gs.folderid + "' in parents").Do()
+	if err != nil {
+		return false, err
+	}
+	return len(folderlist.Items) > 0, nil
+}
+
+func (gs *GDriveStorage) put(f *drive.File, reader *io.PipeReader, done chan error) {
+	var err error
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	gs.connectionsChan <- 0
+
+	for n := 0; n < 5; n++ {
+		_, err = gs.service.Files.Insert(f).Media(reader).Do()
+		if err != nil {
+			if e, ok := err.(*googleapi.Error); ok && e.Code == 403 && strings.EqualFold(e.Message, "Rate Limit Exceeded") {
+				wait_ms := time.Duration(2^n)*time.Second + time.Duration(rnd.Int31n(1000))*time.Millisecond
+				time.Sleep(wait_ms)
+				continue
+			}
+			fmt.Println("closing")
+			fmt.Println(reflect.TypeOf(err).Name())
+			fmt.Println(err)
+			reader.CloseWithError(err)
+			<- gs.connectionsChan
+			done <- err
+			return
+		} else {
+			reader.Close()
+			<- gs.connectionsChan
+			done <- nil
+			return
+		}
+	}
+	fmt.Println("closing")
+	fmt.Println(reflect.TypeOf(err).Name())
+	reader.CloseWithError(err)
+	<- gs.connectionsChan
+	done <- err
 }
 
 func (gs *GDriveStorage) Put(hash string, done chan error) (w io.WriteCloser, e error) {
@@ -117,19 +175,61 @@ func (gs *GDriveStorage) Put(hash string, done chan error) (w io.WriteCloser, e 
 
 	reader, writer := io.Pipe()
 
-	go func() {
-		_, err := gs.service.Files.Insert(f).Media(reader).Do()
-		if err != nil {
-			reader.CloseWithError(err)
-		}
-		done <- err
-	}()
+	go gs.put(f, reader, done)
 
-	return writer, nil
+	limitedWriter := flowcontrol.NewWriter(writer, 1024/16*gs.max_kbps)
+	limitedWriter.SetBlocking(true)
+
+	return limitedWriter, nil
+}
+
+//should only be called if you have acquired the(a) lock for the GDrive storage
+func (gs *GDriveStorage) getFolderList(hash string) (*drive.FileList, error) {
+	var err error
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for n := 0; n < 5; n++ {
+		folderlist, err := gs.service.Files.List().Q("mimeType = 'application/pgp-encrypted' and title = '" + hash + "' and '" + gs.folderid + "' in parents").Do()
+		if err != nil {
+			if e, ok := err.(*net.DNSError); ok && strings.EqualFold(e.Err, "no such host") {
+				wait_ms := time.Duration(2^n)*time.Second + time.Duration(rnd.Int31n(1000))*time.Millisecond
+				time.Sleep(wait_ms)
+				continue
+			}
+			return nil, err
+		} else {
+			return folderlist, nil
+		}
+	}
+	return nil, err
+}
+
+//should only be called if you have acquired the(a) lock for the GDrive storage
+func (gs *GDriveStorage) getFile(url string) (*http.Response, error) {
+	var err error
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for n := 0; n < 5; n++ {
+		request, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return nil, err
+		}
+		response, err := gs.transport.RoundTrip(request)
+		if err != nil {
+			if e, ok := err.(*net.DNSError); ok && strings.EqualFold(e.Err, "no such host") {
+				wait_ms := time.Duration(2^n)*time.Second + time.Duration(rnd.Int31n(1000))*time.Millisecond
+				time.Sleep(wait_ms)
+				continue
+			}
+			return nil, err
+		} else {
+			return response, nil
+		}
+	}
+	return nil, err
 }
 
 func (gs *GDriveStorage) Get(hash string) (io.ReadCloser, error) {
-	folderlist, err := gs.service.Files.List().Q("mimeType = 'application/pgp-encrypted' and title = '" + hash + "' and '" + gs.folderid + "' in parents").Do()
+	gs.connectionsChan <- 0
+	folderlist, err := gs.getFolderList(hash)
 	if err != nil {
 		return nil, err
 	}
@@ -142,14 +242,9 @@ func (gs *GDriveStorage) Get(hash string) (io.ReadCloser, error) {
 		return nil, errors.New(fmt.Sprintf("Error: content not found for '%s'", hash))
 	}
 
-	request, err := http.NewRequest("GET", downloadUrl, nil)
-	if err != nil {
-		return nil, err
-	}
-	response, err := gs.transport.RoundTrip(request)
-	if err != nil {
-		return nil, err
-	}
+	response, err := gs.getFile(downloadUrl)
+
+	<- gs.connectionsChan
 
 	return response.Body, nil
 }
